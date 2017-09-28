75516b99a335994188c07cbeff9660d78dd44420
Kamil Samigullin <kamil@samigullin.info>
2017-09-28 08:46:03 +0300

    add storage interface

--- contract.go
+++ contract.go
@@ -3,11 +3,24 @@ package stream
 // Aggregator is one entry point for many different sources.
 type Aggregator interface {
 	Source
-	Add(Source)
+
+	Add(src Source)
+	Remove(src Source)
+	Get(id string)
+	All() []Source
 }
 
 // Source describes a source can be presented in Atom 1.0 and RSS 2.0.
 type Source interface {
+	ID() string
 	Atom() Atom
 	RSS() RSS
+}
+
+// Storage describes a data access object behavior.
+type Storage interface {
+	Store(src Aggregator)
+	Remove(src Aggregator)
+	Load() []Aggregator
+	LoadByID(id string) Aggregator
 }
